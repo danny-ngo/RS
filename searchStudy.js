93e902a26946a860a1c470b2fb5c02b666783c53
Danny Ngo <dannyngo.1199@gmail.com>
2019-01-13 03:23:43 -0500

    added js for searching studies through api

---
+++ searchStudy.js
@@ -0,0 +1,39 @@
+/*
+ * TO-DO: Connect front-end to js
+ */
+
+// search endpoint urls
+const studyByIdURL = 'https://research-stream.herokuapp.com/study/id/'
+const ethicsClearURL = 'https://research-stream.herokuapp.com/study/EthicsClearance/'
+const paramSearchURL = 'https://research-stream.herokuapp.com/study/'
+
+// list of possible parameters to search by
+const paramList = ['name', 'location', 'lab', 'researcher'];
+
+// test-case id
+const id = '5c37c1ada991b25288c06088'
+
+// search for either study or ethics clearance by id
+function searchByID(url, id) {
+  fetch(url + id, {
+    method: "GET",
+  }).then(response => response.text())
+  .then(response => console.log('Search by ID:', response))
+  .then(error => error.log('Error:', error))
+}
+
+
+searchByID(ethicsClearURL, id);
+
+
+// serch for studies by parameter
+function searchByParam(urlRoot, paramType, paramVal) {
+  fullURL = urlRoot + paramType + '/' + paramVal;
+  fetch(fullURL, {
+    method: "GET",
+  }).then(response => response.text())
+  .then(response => console.log('Search by Parameter:', response))
+  .then(error => error.log('Error:', error))
+}
+
+searchByParam(paramSearchURL, paramList[3], 'Wally West');
