5253302da3a1b750e35f3f729575e73bb506e0c5
Danny Ngo <dannyngo.1199@gmail.com>
2019-01-13 22:03:24 -0500

    added code for accessing response data

--- searchStudy.js
+++ searchStudy.js
@@ -13,27 +13,61 @@ const paramList = ['name', 'location', 'lab', 'researcher'];
 // test-case id
 const id = '5c37c1ada991b25288c06088'
 
-// search for either study or ethics clearance by id
-function searchByID(url, id) {
-  fetch(url + id, {
+const res = {};
+
+// search for study by id
+function searchStudyByID(id) {
+  fetch(studyByIdURL + id, {
     method: "GET",
-  }).then(response => response.text())
-  .then(response => console.log('Search by ID:', response))
-  .then(error => console.error('Error:', error))
+  }).then(response => response.json())
+  .then(function(response) {
+    console.log('Search study by id:');
+    console.log('inside function element by element:');
+    for (let ele in response) {
+      res[ele] =  response[ele];
+      console.log(ele + ':', res[ele]);
+    }
+    console.log('specific element: ', res.name);  //example of accessing a specific element
+    console.log('specific element 2:', res['lab']); // another example of accessing a specific element
+    console.log('inside function as string:', JSON.stringify(res)); // entire json as string
+  })
+  // response and res empty outside of block above, line below outputs empty {}
+  //.then(response => console.log(JSON.stringify(response)))
+  .catch(error => console.error('Error:', error))
 }
 
+// search for ethics clearance by id
+function searchEthicsClear(id) {
+  fetch(ethicsClearURL + id, {
+    method: "GET",
+  }).then(response => response.body)
+  .then(response => console.log('Search for ethics clearance: ', response))
+  .catch(error => console.error('Error:', error))
+}
 
-searchByID(ethicsClearURL, id);
+searchStudyByID(id);
+
+searchEthicsClear(id);
 
 
-// serch for studies by parameter
+
+// search for studies by parameter
 function searchByParam(urlRoot, paramType, paramVal) {
   let fullURL = urlRoot + paramType + '/' + paramVal;
   fetch(fullURL, {
     method: "GET",
-  }).then(response => response.text())
-  .then(response => console.log('Search by Parameter:', response))
-  .then(error => console.error('Error:', error))
+  }).then(response => response.json())
+  .then(function(response) {
+    console.log('Search by parameter:');
+    let j = []
+    for(let ele in response) {
+      j.push(response[ele]);
+    }
+    console.log('First JSON in array:', j[0]);  // accessing specific JSON in array (could stringify)
+    console.log('Specific element in JSON:', j[0].name); // accessing specific element of a specific JSON in array
+    console.log('Specific element in JSON:', j[0]['lab']);  // another example of accessing specific element
+  })
+  .catch(error => console.error('Error:', error))
 }
 
 searchByParam(paramSearchURL, paramList[3], 'Wally West');
