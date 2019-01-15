a64a3202b7160b4ba03f017e2323e4fcc3b6e6f2
Danny Ngo <dannyngo.1199@gmail.com>
2019-01-15 01:14:55 -0500

    updated login.js

--- login.js
+++ login.js
@@ -1,21 +1,13 @@
 // URL for login request
 const loginURL = 'https://research-stream.herokuapp.com/user/login';
 
-// finding neccessary elements in DOM
-const email =  document.querySelector('#inputEmail').value;
-const password = document.querySelector('#inputPassword').value;
-const loginBtn = document.querySelector('#hello');
-
-// adding event listener to "login button"
-loginBtn.addEventListener('click', login)
-
-const data = {
-  "email": email,
-  "password": password,
-}
-
 // accessing login endpoint
-function login(event) {
+function login() {
+  const data = {
+    "email": document.querySelector('#inputEmail').value,
+    "password": document.querySelector('#inputPassword').value,
+  }
+  console.log(data);  // FOR TESTING
   fetch(loginURL, {
     method: "POST",
     headers: {
